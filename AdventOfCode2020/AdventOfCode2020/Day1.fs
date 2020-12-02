53617757871c454bf1050c262904115ab91f4cce
Dave Warry <dave@warry.org.uk>
2020-12-02 09:55:41 +0000

    Day 1 and 2 solutions

---
+++ AdventOfCode2020/AdventOfCode2020/Day1.fs
@@ -0,0 +1,41 @@
+﻿module Day1
+
+open System.IO
+
+let readFile (path: string) = 
+  File.ReadAllLines(path)
+    |> Array.map int
+    |> Array.sort
+
+let solve (path: string) = 
+  let findMatch data = Array.find (fun x -> System.Array.BinarySearch(data, 2020 - x) > 0) data
+  let calcResult first = (2020 - first) * first
+  readFile path
+    |> findMatch
+    |> calcResult
+    
+let solve3 (path: string) = 
+  let tailArray a n = 
+    let aLength = Array.length a
+    if aLength > n then
+      Array.sub a n (aLength - n)
+    else
+      Array.empty
+
+  let findSecond data target = Array.tryFind (fun x -> System.Array.BinarySearch(data, target - x) > 0) data
+  let findMatch (data: int[]) = 
+    data
+      |> Array.mapi (fun i x -> 
+           let first = data.[i]
+           let target = 2020 - first
+           let second = findSecond (tailArray data (i + 1)) target
+           let third = Option.map (fun x -> target - x) second
+           match second, third with
+           | Some x, Some y -> Some (first * x * y)
+           | _ -> None ) 
+      |> Array.find Option.isSome
+
+  readFile path 
+    |> findMatch
+        
+
---
+++ AdventOfCode2020/AdventOfCode2020/Day2.fs
@@ -0,0 +1,34 @@
+﻿module Day2
+
+open System.IO
+open System.Text.RegularExpressions
+
+type PasswordRecord = {min: int; max: int; ch: char; password: string}
+
+let parseLine (line: string) = 
+  let m = Regex.Match(line, @"^(\d+)\-(\d+) ([a-z]): ([a-z]+)")
+  if m.Success then
+    Some { min = int m.Groups.[1].Value; 
+           max = int m.Groups.[2].Value; 
+           ch = m.Groups.[3].Value.[0]; 
+           password = m.Groups.[4].Value }
+  else
+    None
+
+let isValid (pswd: PasswordRecord): bool =
+  let chCount = 
+    pswd.password.ToCharArray() 
+      |> Array.fold (fun count ch -> if ch = pswd.ch then (count + 1) else count) 0
+  chCount >= pswd.min && chCount <= pswd.max
+
+let isValid2 (pswd: PasswordRecord): bool = 
+  (pswd.password.[pswd.min - 1] = pswd.ch) <> (pswd.password.[pswd.max - 1] = pswd.ch)
+
+let readFile (path: string) =
+  File.ReadAllLines(path) 
+    |> Array.choose parseLine
+
+let countValid (passwords: PasswordRecord[]) (predicate: PasswordRecord -> bool) =
+  passwords |> Array.fold (fun count pswd -> if predicate pswd then count + 1 else count) 0
+
+
