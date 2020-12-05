c47455140e015d218b2640158f9a81a5510122eb
Dave Warry <dave@warry.org.uk>
2020-12-05 08:24:17 +0000

    Day 5 solution

---
+++ AdventOfCode2020/AdventOfCode2020/Day5.fs
@@ -0,0 +1,32 @@
+﻿module Day5
+
+open System.IO
+
+let readFile path = 
+  File.ReadAllLines(path)
+
+// description is really just a convoluted way of describing binary where B and R are 1; F and L = 0
+// Was going to use System.Collections.Specialized.BitVector32 to do the bit-twiddling, 
+// but couldn't get F# to assign to the Item property. 
+let findSeat (locator: string) = 
+    locator.ToCharArray()
+    |> Array.mapi (fun i ch -> let pow = locator.Length - 1 - i
+                               let v = if (ch = 'B' || ch = 'R') then 1 else 0
+                               (v <<< pow))
+    |> Array.fold (+) 0
+      
+let findMaxSeat path = 
+    path 
+    |> readFile 
+    |> Array.map findSeat
+    |> Array.max
+
+let findMySeat path = 
+    path
+    |> readFile
+    |> Array.map findSeat
+    |> Array.sort
+    |> Array.pairwise
+    |> Array.find (fun p -> (snd p) - (fst p) = 2)
+    |> (fun p -> (fst p) + 1)
+
