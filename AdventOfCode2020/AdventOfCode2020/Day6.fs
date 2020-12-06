dae3f90f12d00638f9efd0c5ee9bc02ec4d3c3a9
Dave Warry <dave@warry.org.uk>
2020-12-06 08:54:28 +0000

    Day 6 solutions

---
+++ AdventOfCode2020/AdventOfCode2020/Day6.fs
@@ -0,0 +1,27 @@
+﻿module Day6
+
+open System.IO
+
+let readFile path =
+    let text = File.ReadAllText(path)
+    let separator = System.Environment.NewLine + System.Environment.NewLine
+    text.Split([| separator |], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun x -> x.Trim())
+
+
+let processGroup (groupData: string) = 
+   groupData.ToCharArray()
+   |> Array.fold(fun (res: Set<char>) (ch: char) -> if ch >= 'a' then res.Add ch else res ) Set.empty<char>
+
+
+let processGroup2 (groupData: string) = 
+   groupData.Split([| System.Environment.NewLine |], System.StringSplitOptions.None)
+   |> Array.map (fun x -> Set.ofArray (x.ToCharArray()))
+   |> Array.fold (fun ss x -> Set.intersect ss x) (Set(seq {'a' .. 'z'}))
+
+let countAnswersForGroups path = 
+   readFile path
+   |> Array.map processGroup2
+   |> Array.map (fun x -> Set.count x)
+
+
